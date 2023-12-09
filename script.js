b13330363f0bd7317340d039aff1a46b34daf21d
Bubbabunt <153339439+Bubbabunt@users.noreply.github.com>
2023-12-10 00:59:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,50 @@
-function animateGreetings() {
-  // Use anime.js for animations
-  anime({
-    targets: '.greeting-card',
-    translateX: [0, 50],
-    opacity: [1, 0],
-    easing: 'easeInOutQuad',
-    duration: 1000,
-    complete: function(anim) {
-      // Animation complete callback
-      document.getElementById('greetingCard').innerHTML = '<h1>🎉 Happy Birthday! 🎉</h1><p>Wishing you a fantastic day filled with joy and celebration.</p>';
-      anime({
-        targets: '.greeting-card',
-        translateX: [50, 0],
-        opacity: [0, 1],
-        easing: 'easeInOutQuad',
-        duration: 1000
-      });
-    }
-  });
+#container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 100vw;
+    height: 100vh;
+}
 
-  // You can add more animations or effects as needed
+h1 {
+    font-size: 3em;
+    margin-bottom: 0.5em;
+}
+
+#message {
+    font-size: 1.5em;
+    margin-bottom: 2em;
+}
+
+#decorations {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    width: 80%;
+}
+
+.decoration {
+    width: 100px;
+    height: 100px;
+    margin: 10px;
+    cursor: grab;
+}
+
+.drag-target {
+    touch-action: none;
+}
+
+/* Animations for visual and haptic feedback */
+
+@keyframes shake {
+    0% { transform: rotate(0deg) scale(1); }
+    50% { transform: rotate(10deg) scale(1.1); }
+    100% { transform: rotate(0deg) scale(1); }
+}
+
+@keyframes confetti {
+    0% { transform: scale(1); }
+    50% { transform: scale(1.5); }
+    100% { transform: scale(1); }
 }
